8449fcb5bd3e584ad53bc4e7cc01298ada222bb8
iabubakartahir <77561458+iabubakartahir@users.noreply.github.com>
2025-12-14 22:17:15 +0500

    Update route.ts

--- nextjs-dashboard-group4/app/seed/route.ts
+++ nextjs-dashboard-group4/app/seed/route.ts
@@ -1,7 +1,7 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET() { 
   try {
     // Create tables
     await sql`
